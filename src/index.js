bf984d96096284a4e1abb4c649d18a6a0d41b5ad
github username <github email address>
2019-05-01 10:31:46 -0400

    fetch and render method

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 // It might be a good idea to add event listener to make sure this file 
 // only runs after the DOM has finshed loading.
+
+document.addEventListener('DOMContentLoaded', init())
+
+function init() {
+    getQuotes()
+}
+
+function getQuotes() {
+    fetch("http://localhost:3000/quotes")
+    .then(res => res.json()).then(data => data.forEach(renderQuote))
+}
+
+function renderQuote(quote) {
+    console.log(quote)
+    html = ` <li class='quote-card'>
+    <blockquote class="blockquote">
+      <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="blockquote-footer">Someone famous</footer>
+      <br>
+      <button class='btn-success'>Likes: <span>0</span></button>
+      <button class='btn-danger'>Delete</button>
+    </blockquote>
+  </li> `
+    const quoteContainer = document.querySelector('#quote-list')
+
+}
